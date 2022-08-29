234f1debc64e9e79d6868512a4c6ce520eae9e43
furrrax <cjuiceone@gmail.com>
2022-08-29 20:18:22 +0300

    Авторизация через context-api

--- src/pages/PageLogin.jsx
+++ src/pages/PageLogin.jsx
@@ -1,42 +1,73 @@
-import React from "react";
+import {React, useContext, useState} from "react";
 import logo from '../img/loft-taxi-logo.svg';
 import PropTypes from "prop-types";
+import { AuthContext } from "../AuthContext";
 
 function PageLogin(props) {
 
     const {setPage} = props;
+    const {logIn, isLoggedIn} = useContext(AuthContext);
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const emailHandleChange = (event) => {
+        setEmail(event.target.value);
+        console.log(event.target.value);
+    };
+
+    const passwordHandleChange = (event) => {
+        setPassword(event.target.value);
+        console.log(event.target.value);
+    };
+
+    const submitHandle = (event) => {
+        event.preventDefault();
+        logIn(email, password);
+    };
 
     return (
-        <section className="main">
-            <h1>Страница входа</h1>
-            <div className="container">
-                <img className="main__bg" alt="фон"/>
-                <div className="main__sidebar">
-                    <div className="main__sidebar__logo">
-                        <img src={logo} className="main__sidebar__logo__pic" alt="logo" />
-                        <span className="main__sidebar__logo__text">loft<span className="main__sidebar__logo__text--yellow">taxi</span></span>
-                    </div>
-                </div>
-                <div className="enter-form">
-                    <h3 className="enter-form__title">Войти</h3>
-                    <form className="enter-form__content">
-                        <div className="enter-form__content__inputs">
-                            <div className="input__wrap">
-                                <label htmlFor="email" className="input__title">Email</label>
-                                <input id="email" type="email" name="email" className="input__field" placeholder="mail@mail.ru"></input>
-                            </div>
-                            <div className="input__wrap">
-                                <label htmlFor="password" className="input__title">Password</label>
-                                <input id="password" type="password" name="password" className="input__field" placeholder="*************"></input>
-                            </div>
+        <>
+            <section className="main">
+                <h1>Страница входа</h1>
+                <div className="container">
+                    <img className="main__bg" alt="фон"/>
+                    <div className="main__sidebar">
+                        <div className="main__sidebar__logo">
+                            <img src={logo} className="main__sidebar__logo__pic" alt="logo" />
+                            <span className="main__sidebar__logo__text">loft<span className="main__sidebar__logo__text--yellow">taxi</span></span>
                         </div>
-                        <button className="enter-form__content__link">Забыли пароль?</button>
-                        <button onClick={() => setPage('pageMap')} className="button-submit" type="submit">Войти</button>
-                        <button onClick={() => setPage('pageReg')} className="enter-form__content__link">Новый пользователь? <span className="enter-form__content__link--yellow">Регистрация</span></button>
-                    </form>
+                    </div>
+                    {isLoggedIn ? (
+                        <p>
+                            Вы залогинились{" "}
+                            <button onClick={() => setPage('pageMap')}>
+                                Перейти на карту
+                            </button>
+                        </p>
+                    ) : (
+                        <div className="enter-form">
+                            <h3 className="enter-form__title">Войти</h3>
+                            <form className="enter-form__content" onSubmit={submitHandle}>
+                                <div className="enter-form__content__inputs">
+                                    <div className="input__wrap">
+                                        <label htmlFor="email" className="input__title">Email</label>
+                                        <input value={email} onChange={emailHandleChange} id="email" type="email" name="email" className="input__field" placeholder="mail@mail.ru"></input>
+                                    </div>
+                                    <div className="input__wrap">
+                                        <label htmlFor="password" className="input__title">Password</label>
+                                        <input value={password} onChange={passwordHandleChange} id="password" type="password" name="password" className="input__field" placeholder="*************"></input>
+                                    </div>
+                                </div>
+                                <button className="enter-form__content__link">Забыли пароль?</button>
+                                <button /* onClick={() => setPage('pageMap')} */ className="button-submit" type="submit">Войти</button>
+                                <button onClick={() => setPage('pageReg')} className="enter-form__content__link">Новый пользователь? <span className="enter-form__content__link--yellow">Регистрация</span></button>
+                            </form>
+                        </div>
+                    )}
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     )
 };
 
--- src/pages/PageProfile.jsx
+++ src/pages/PageProfile.jsx
@@ -1,45 +1,56 @@
-import React from "react";
+import {React, useContext} from "react";
 import logo from '../img/loft-taxi-logo.svg';
 import MapInner from "../components/MapInner";
 import PopupProfile from "../components/popup/PopupProfile";
 import PopupProfileSuccess from "../components/popup/PopupProfileSuccess";
 import PropTypes from "prop-types";
+import { AuthContext } from "../AuthContext";
 
 function PageProfile (props) {
 
     const {setPage} = props;
+    const {logOut, isLoggedIn} = useContext(AuthContext);
 
     return (
         <section className="inner">
-            <div className="container">
-                <header className="header">
-                    <div className="header__logo">
-                        <img src={logo} className="header__logo__pic" alt="logo" />
-                        <div className="header__logo__text">loft<span className="header__logo__text--yellow">taxi</span></div>
+            {!isLoggedIn ? (
+                        <p>
+                            Вы вышли{" "}
+                            <button onClick={() => setPage('pageLogin')}>
+                                Вернуться на главную
+                            </button>
+                        </p>
+                ) : (
+                    <div className="container">
+                        <header className="header">
+                            <div className="header__logo">
+                                <img src={logo} className="header__logo__pic" alt="logo" />
+                                <div className="header__logo__text">loft<span className="header__logo__text--yellow">taxi</span></div>
+                            </div>
+                            <nav className="header__nav">
+                                <ul className="header__nav__list">
+                                    <li className="header__nav__item header__nav__item">
+                                        <button onClick={() => setPage('pageMap')} className="header__nav__item__link">Карта</button>
+                                    </li>
+                                    <li className="header__nav__item header__nav__item">
+                                        <button onClick={() => setPage('pageProfile')} className="header__nav__item__link">Профиль</button>
+                                    </li>
+                                    <li className="header__nav__item header__nav__item">
+                                        <button onClick={logOut} className="header__nav__item__link">Выход</button>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </header>
+                        <div className="profile">
+                            <h1>Страница Профиля</h1>
+                            <div class="profile__map-wrapper">
+                                <MapInner />
+                            </div>
+                            <PopupProfile />
+                            <PopupProfileSuccess />
+                        </div>
                     </div>
-                    <nav className="header__nav">
-                        <ul className="header__nav__list">
-                            <li className="header__nav__item header__nav__item">
-                                <button onClick={() => setPage('pageMap')} className="header__nav__item__link">Карта</button>
-                            </li>
-                            <li className="header__nav__item header__nav__item">
-                                <button onClick={() => setPage('pageProfile')} className="header__nav__item__link">Профиль</button>
-                            </li>
-                            <li className="header__nav__item header__nav__item">
-                                <button onClick={() => setPage('pageLogin')} className="header__nav__item__link">Выход</button>
-                            </li>
-                        </ul>
-                    </nav>
-                </header>
-                <div className="profile">
-                    <h1>Страница Профиля</h1>
-                    <div class="profile__map-wrapper">
-                        <MapInner />
-                    </div>
-                    <PopupProfile />
-                    <PopupProfileSuccess />
-                </div>
-            </div>
+                )}
         </section>
     );
 }
