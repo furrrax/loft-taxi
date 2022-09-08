7f59989838483b53e7c59388574618a3fd5a6b40
furrrax <cjuiceone@gmail.com>
2022-09-08 15:47:09 +0300

    Middleware fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,18 @@
-import { configureStore, applyMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 import { userReducer } from "./reducers/user";
 import { authMiddleware } from "./middlwares/auth";
 
-const rootReducer = combineReducers({user: userReducer});
-const middleware = applyMiddleware(authMiddleware);
+const rootReducer = combineReducers({
+    user: userReducer,
+});
+
+//const middleware = applyMiddleware(authMiddleware);
 //export const store = configureStore({reducer: rootReducer});
-export const store = configureStore({reducer: rootReducer}, middleware);
+//export const store = configureStore({reducer: rootReducer}, middleware);
+
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authMiddleware),
+});
