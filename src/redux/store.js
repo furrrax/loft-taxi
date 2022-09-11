1750aa77d5d763b59b4b36200e3dd8c6d9a52e1a
furrrax <cjuiceone@gmail.com>
2022-09-11 15:19:29 +0300

    Инициализация redux-saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from 'redux-saga';
+import { rootSagas } from "./sagas";
 import { combineReducers } from "redux";
 import { userReducer } from "./reducers/user";
 import { authMiddleware, regMiddleware } from "./middlwares/auth";
@@ -7,8 +9,15 @@ export const rootReducer = combineReducers({
     user: userReducer,
 });
 
+export const sagaMiddleware = createSagaMiddleware();
+
 export const store = configureStore({
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authMiddleware).concat(regMiddleware),
+    getDefaultMiddleware()
+    .concat(authMiddleware)
+    .concat(regMiddleware)
+    .concat(sagaMiddleware),
 });
+
+sagaMiddleware.run(rootSagas);
