e6575df40af0eb95ca80f06b54a9fc587012599a
furrrax <cjuiceone@gmail.com>
2022-09-18 16:44:51 +0300

    Стили для активного элемента меню

--- src/pages/PageProfile.jsx
+++ src/pages/PageProfile.jsx
@@ -3,7 +3,7 @@ import logo from '../img/loft-taxi-logo-inner.svg';
 import MapInner from "../components/MapInner";
 import PropTypes from "prop-types";
 
-import { Link, Navigate, Outlet } from "react-router-dom";
+import { Link, Navigate, Outlet, NavLink } from "react-router-dom";
 
 import { useSelector, useDispatch } from "react-redux";
 import { logOut } from "../redux/actions/user";
@@ -30,14 +30,14 @@ function PageProfile () {
                         <nav className="header__nav">
                             <ul className="header__nav__list">
                                 <li className="header__nav__item">
-                                    <Link to="/map">
+                                    <NavLink to="/map">
                                         <button className="header__nav__item__link">Карта</button>
-                                    </Link>
+                                    </NavLink>
                                 </li>
                                 <li className="header__nav__item">
-                                    <Link to="/profile">
+                                    <NavLink to="/profile">
                                         <button className="header__nav__item__link">Профиль</button>
-                                    </Link>
+                                    </NavLink>
                                 </li>
                                 <li className="header__nav__item header__nav__item">
                                     <button onClick={logOff} className="header__nav__item__link">Выход</button>
--- src/pages/layouts/mapLayout.jsx
+++ src/pages/layouts/mapLayout.jsx
@@ -5,7 +5,7 @@ import MapInner from "../../components/MapInner";
 import { useDispatch } from "react-redux";
 import { logOut } from "../../redux/actions/user";
 
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, NavLink } from "react-router-dom";
 
 function MapLayout (component) {
 
@@ -27,14 +27,14 @@ function MapLayout (component) {
                     <nav className="header__nav">
                         <ul className="header__nav__list">
                             <li className="header__nav__item">
-                                <Link to="/map">
+                                <NavLink to="/map">
                                     <button className="header__nav__item__link">Карта</button>
-                                </Link>
+                                </NavLink>
                             </li>
                             <li className="header__nav__item">
-                                <Link to="/profile">
+                                <NavLink to="/profile">
                                     <button className="header__nav__item__link">Профиль</button>
-                                </Link>
+                                </NavLink>
                             </li>
                             <li className="header__nav__item">
                                 <button onClick={logOff} className="header__nav__item__link">Выход</button>
